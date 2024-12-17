b2111819c393d54e8c9b2238a6ab495ba5d56c2d
Nived R S <nivedrsalini@gmail.com>
2024-12-17 22:47:52 +0530

    update schedule cards

--- lib/UI/Screens/Schedule/Widgets/generateScheduleCardList.dart
+++ lib/UI/Screens/Schedule/Widgets/generateScheduleCardList.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:excelapp/Models/schedule_model.dart';
 import 'package:excelapp/UI/Themes/colors.dart';
+import 'package:excelapp/UI/Themes/gradient.dart';
 import 'package:excelapp/UI/Themes/profile_themes.dart';
 import 'package:intl/intl.dart';
 import 'package:excelapp/UI/Screens/EventPage/eventPage.dart';
@@ -104,7 +105,7 @@ class ScheduleEvent extends StatelessWidget {
                     padding: EdgeInsets.all(17),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
-                        color: Color(0xffFBFFFF),
+                        gradient: primaryGradientHorizontal(),
                         border: Border.all(
                             color: ExcelTheme.aevaDark.withOpacity(0.1))),
                     margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
@@ -126,7 +127,7 @@ class ScheduleEvent extends StatelessWidget {
                             child: Container(
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(21),
-                            color: red100,
+                            color: tertiaryPurple,
                           ),
                           child: Padding(
                             padding: EdgeInsets.all(12.25),
@@ -162,8 +163,10 @@ class ScheduleEvent extends StatelessWidget {
                                               : "") ??
                                       "",
                                   style: TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 14),
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 14,
+                                    color: secondaryColor,
+                                  ),
                                 ),
                                 SizedBox(
                                   height: 8,
@@ -182,7 +185,7 @@ class ScheduleEvent extends StatelessWidget {
                                     "View Event",
                                     style: TextStyle(
                                         fontSize: 11,
-                                        color: red100,
+                                        color: primaryPink,
                                         fontWeight: FontWeight.w600),
                                   ),
                                 ),
