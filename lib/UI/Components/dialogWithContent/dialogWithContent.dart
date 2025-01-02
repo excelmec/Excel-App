5c1f778891b02db7d95a916396da42b4af67bedc
Nived R S <nivedrsalini@gmail.com>
2025-01-02 12:58:31 +0530

    Add Redirection when profile is incomplete

--- lib/UI/Components/dialogWithContent/dialogWithContent.dart
+++ lib/UI/Components/dialogWithContent/dialogWithContent.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../Themes/colors.dart';
+
 dialogWithContent({required Widget child, required context}) {
   showModalBottomSheet(
+      backgroundColor: backgroundBlue,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(40), topRight: Radius.circular(40))),
