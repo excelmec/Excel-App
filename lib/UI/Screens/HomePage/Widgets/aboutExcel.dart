86dd594323ce8c96177564c8a198b66c7f6d9b5e
Nived R S <nivedrsalini@gmail.com>
2025-01-07 12:38:29 +0530

    Reg logic change

--- lib/UI/Screens/HomePage/Widgets/aboutExcel.dart
+++ lib/UI/Screens/HomePage/Widgets/aboutExcel.dart
@@ -66,7 +66,7 @@ class AboutExcelPopUp extends StatelessWidget {
                   children: const <TextSpan>[
                     TextSpan(
                       text:
-                          "\n\nExcel, the nation’s second and South India’s first ever fest of its kind, was started in 2001 by the students of Govt. Model Engineering College. Over the years, Excel has grown exponentially, consistently playing host to some of the most talented students, the most illustrious speakers and the most reputed companies. \n\nNow gearing towards the landmark 24rd edition, Excel continues to march forward. Join us this December to experience the magic of Excel!.",
+                          "\n\nExcel, the nation's second and South India's first ever techno-managerial fest of its kind was started in 2001 by the students of Govt. Model Engineering College, Kochi. Over the years, Excel has grown exponentially, consistently playing host to some of the most talented students, the most coveted guests and the most reputed companies.\nAs we gear up for our 25th edition, Excel continues to push boundaries. Join us this January and experience the magic of a legacy in the making.",
                       style: TextStyle(
                         color: Color(0xFFE4EDEF),
                         fontWeight: FontWeight.w400,
