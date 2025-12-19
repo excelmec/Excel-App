570e106b3fe8c912ce3e8e324bd0fd26e71c70da
Pranav M <pranavm265@gmail.com>
2025-12-19 13:30:35 +0530

    fixes

--- lib/features/discover/widgets/discover_card.dart
+++ lib/features/discover/widgets/discover_card.dart
@@ -22,6 +22,7 @@ class DiscoverCard extends StatelessWidget {
           Row(
             children: [
               CachedImage(
+                key: ValueKey('${event.id}-${event.icon}'),
                 imageUrl: event.icon,
                 width: 60,
                 height: 60,
